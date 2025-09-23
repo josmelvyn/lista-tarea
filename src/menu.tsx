68420cf80c0a72712df63e1b8e721cc15e0abaee
josmelvyn <josmelvyn@outlook.es>
2025-09-23 19:21:31 -0400

    se comento el codigo para luego arreglarlo

--- src/menu.tsx
+++ src/menu.tsx
@@ -1,19 +1,19 @@
-import {useEffect,useState } from "react";
-import './menu.css'
+// //import {useEffect,useState } from "react";
+// import './menu.css'
 
-interface MenuItem {
-  id: string;
-  label: string;
-  icon?: string;
-  action?: () => void;
-  subMenu?: MenuItem[];
-  separator?: boolean;
-  disabled?: boolean;
-}
+// interface MenuItem {
+//   id: string;
+//   label: string;
+//   icon?: string;
+//   action?: () => void;
+//   subMenu?: MenuItem[];
+//   separator?: boolean;
+//   disabled?: boolean;
+// }
 
-const mainMenu: MenuItem[] = [
-    {id:"file", label:"File", subMenu:[
-        {id:"new", label:"New", icon:"icon-new", action: () => console.log("new file")},
-]},
+// //const mainMenu: MenuItem[] = [
+//     {id:"file", label:"File", subMenu:[
+//         {id:"new", label:"New", icon:"icon-new", action: () => console.log("new file")},
+// ]},
 
-];
+// ];
